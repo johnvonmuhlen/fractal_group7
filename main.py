cd6035842bfe15e6753f1127afc0e66c2763c245
johnvonmuhlen <johnvonmuhlen@gmail.com>
2023-11-14 11:15:45 +0100

    basic tkinter setup

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 #main code
+import tkinter as tk
+
+# Create the main window
+window = tk.Tk()
+
+# Set the window title
+window.title("Hello Tkinter!")
+
+# Create a label widget
+label = tk.Label(window, text="Hello, Tkinter!")
+
+# Pack the label widget into the window
+label.pack()
+
+# Start the Tkinter event loop
+window.mainloop()
