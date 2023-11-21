cb6bd3e3c51a2b1facb1a1e053285eb4f9e02790
johnvonmuhlen <johnvonmuhlen@gmail.com>
2023-11-21 11:18:14 +0100

    start basic tree

--- main.py
+++ main.py
@@ -1,17 +1,46 @@
-#main code
-import tkinter as tk
+from tkinter import *
+import random
+import math
 
-# Create the main window
-window = tk.Tk()
+#functions to update the tree thorugh user input not wokring
+#TODO: fix user input
 
-# Set the window title
-window.title("Hello Tkinter!")
+def draw_tree(canvas, x1, y1, angle, length, depth, scale):
+ if depth == 0:
+   return
 
-# Create a label widget
-label = tk.Label(window, text="Hello, Tkinter!")
+ x2 = x1 + length * math.cos(math.radians(angle)) * scale
+ y2 = y1 + length * math.sin(math.radians(angle)) * scale
 
-# Pack the label widget into the window
-label.pack()
+ canvas.create_line(x1, y1, x2, y2, width=3, smooth=True)
 
-# Start the Tkinter event loop
-window.mainloop()
+ canvas.after(500, draw_tree, canvas, x2, y2, angle - random.randint(10, 45), length * 0.7, depth - 1, scale)
+ canvas.after(500, draw_tree, canvas, x2, y2, angle + random.randint(10, 45), length * 0.7, depth - 1, scale)
+
+def update_scale(scale):
+ scale_factor = float(scale) / 100
+ canvas.delete("all")
+ draw_tree(canvas, 400, 600, -90, 300, 10, scale_factor)
+
+def update_tree(event):
+ value = int(entry.get())
+ canvas.delete("all")
+ draw_tree(canvas, 400, 600, -90, 300, value, 1)
+
+root = Tk()
+canvas = Canvas(root, width=800, height=600)
+canvas.pack()
+root.title("Group 7 Tree Fractal")
+
+scale = Scale(root, from_=50, to=200, orient='horizontal')
+scale.pack()
+
+entry = Entry(root)
+entry.pack()
+
+button = Button(root, text="Draw Tree", command=update_tree)
+button.pack()
+
+update_scale(scale.get())
+
+root.mainloop()
