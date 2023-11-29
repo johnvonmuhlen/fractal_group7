a7045dd7cfca590b0ecb724fddc27d6629fcb06a
johnvonmuhlen <johnvonmuhlen@gmail.com>
2023-11-29 17:17:45 +0100

    quality of life changes/added depth slider

--- generate_tree.py
+++ generate_tree.py
@@ -2,25 +2,39 @@ import random
 import math
 from tkinter import *
 
-def generate_tree(canvas, x1, y1, angle, length, depth, scale, button, root, user_selections, width=7):
-    if depth == 0:
-        button.config(text="Clear Tree", state='normal')
+def generate_tree(canvas, x1, y1, angle, length, button, root, user_selections, width=7):
+    #base case
+    if user_selections['depth'] == 0:
+        button.configure(text="Clear Tree", state='normal')
         return
     
-    if button['text'] == 'Clear Tree':
+    #user can clear the tree after its done generating
+    if button.cget("text") == 'Clear Tree':
          canvas.delete('all')
-         button.config(text="Start Generating New Tree", state='normal')
+         button.configure(text="Start Generating New Tree", state='normal')
          return
     
-    button.config(text="Generating Tree...", state='disabled')
-    
-    
-    x2 = x1 + length * math.cos(math.radians(angle)) * scale
-    y2 = y1 + length * math.sin(math.radians(angle)) * scale
+    #disable button while tree is generating
+    button.configure(text="Generating Tree...", state='disabled')
 
+    #math to draw tree
+    x2 = x1 + length * math.cos(math.radians(angle)) * (user_selections['scale'] * 0.01)
+    y2 = y1 + length * math.sin(math.radians(angle)) * (user_selections['scale'] * 0.01)
+    
+    #draws each line
     canvas.create_line(x1, y1, x2, y2, width=width, smooth=True)
     
+    #make the branches progressively shorter
     new_width = width * 0.8
-
-    canvas.after(300, generate_tree, canvas, x2, y2, angle - random.randint(10, user_selections['randomization']), length * (user_selections['scale'] * 0.01), depth - 1, scale, button, root, user_selections, new_width)
-    canvas.after(300, generate_tree, canvas, x2, y2, angle + random.randint(10, user_selections['randomization']), length * (user_selections['scale'] * 0.01), depth - 1, scale, button, root, user_selections, new_width)
+    
+    #update depth
+    new_user_selections = {
+        'scale':user_selections['scale'],
+        'randomization':user_selections['randomization'],
+        'season':user_selections['season'],
+        'depth':user_selections['depth'] - 1
+    }
+    
+    #one branch for left and one for right
+    canvas.after(300, generate_tree, canvas, x2, y2, angle - random.randint(10, user_selections['randomization']), length * (user_selections['scale'] * 0.01), button, root, new_user_selections, new_width)
+    canvas.after(300, generate_tree, canvas, x2, y2, angle + random.randint(10, user_selections['randomization']), length * (user_selections['scale'] * 0.01), button, root, new_user_selections, new_width)
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from tkinter import *
 from generate_tree import generate_tree
-
+from customtkinter import *
 import random
 
 root = Tk()
@@ -8,60 +8,100 @@ canvas = Canvas(root,width=800, height=600)
 canvas.pack(fill=BOTH, expand=True, side='right')
 root.title("Group 7 Tree Fractal")
 
+#zoom in/out functions
 def zoom_in(event):
-   canvas.scale(ALL, event.x, event.y, 1.1, 1.1)
+   if generate_tree_button.cget('text') != 'Generating Tree...' or generate_tree_button.cget('text') == 'Clear Tree':
+    canvas.scale(ALL, event.x, event.y, 1.1, 1.1)
 
 def zoom_out(event):
-   canvas.scale(ALL, event.x, event.y, 0.9, 0.9)
+   if generate_tree_button.cget('text') != 'Generating Tree...' or generate_tree_button.cget('text') == 'Clear Tree':
+    canvas.scale(ALL, event.x, event.y, 0.9, 0.9)
 
-# sidebar
+#sidebar
 sidebar = Frame(root, width=200, bg='white', height=500, relief='sunken', borderwidth=4)
 sidebar.pack(expand=True, fill='both', side='left', anchor='nw')
 
-#Button to generate trees
-generate_tree_button = Button(sidebar, text="Start Generating New Tree", command=lambda: generate_tree(canvas, 400, 520, -90, 300, 10, 0.5, generate_tree_button, root, {'scale':slider.get(),
-                                                                                                                                                                         'randomization':slider_2.get(),
-                                                                                                                                                    'season':selected_season.get()}))
+#default tree values passed to generate tree
+starting_tree_values = {'x1': 400,
+                        'y1': 520,
+                        'angle': -90,
+                        'length':300,
+                        }
+
+#Button to generate trees                                                                                        
+generate_tree_button = CTkButton(sidebar, text="Start Generating New Tree", command=lambda: generate_tree(canvas, starting_tree_values['x1'], starting_tree_values['y1'], starting_tree_values['angle'], starting_tree_values['length'], generate_tree_button, root, {'scale':scale_slider.get(), 'randomization':randomization_slider.get(),'season':selected_season.get(), 'depth': round(depth_slider.get() / 10)}))
 generate_tree_button.pack(pady=20)
 
-
-#if generate_tree_button['text'] != 'Generating Tree...' or generate_tree_button['text'] != 'Clear Tree':
+#zoom in commands for clicking
 canvas.bind('<Button-1>', zoom_in)
 canvas.bind('<Button-3>', zoom_out)
 
-#Slider Label
-slider_label = Label(sidebar, text='Choose tree scale')
-slider_label.pack()
+#Depth slider Label
+depth_label = CTkLabel(sidebar, text='Choose tree depth', text_color='black')
+depth_label.pack()
+CTkLabel(sidebar, text='Choices bigger than default can make the program laggy', text_color='black', font=('Arial', 10)).pack()
 
-#Slider to make tree larger/shorter
-slider = Scale(sidebar, from_=0, to=100, orient='horizontal')
-slider.set(70)
-slider.pack()
+#Slider to choose depth
+depth_slider = CTkSlider(sidebar, from_=20, to=150, orientation='horizontal')
+depth_slider.set(100)
+depth_slider.pack()
 
-#Slider Label
-slider_label = Label(sidebar, text='Randomize Tree')
-slider_label.pack()
+#scale Slider Label
+scale_slider_label = CTkLabel(sidebar, text='Choose tree size', text_color='black')
+scale_slider_label.pack()
 
-#Slider to make tree larger/shorter
-slider_2 = Scale(sidebar, from_=0, to=100, orient='horizontal')
-slider_2.set(45)
-slider_2.pack()
+#Slider to choose scale
+scale_slider = CTkSlider(sidebar, from_=40, to=100, orientation='horizontal')
+scale_slider.set(70)
+scale_slider.pack()
+
+#randomization Slider Label
+randomization_slider_label = CTkLabel(sidebar, text='Randomize Tree', text_color='black')
+randomization_slider_label.pack()
+
+#Slider to choose randomization
+randomization_slider = CTkSlider(sidebar, from_=11, to=100, orientation='horizontal')
+randomization_slider.set(45)
+randomization_slider.pack()
 
 #Seasons Label
-seasons_label = Label(sidebar, text='Select Season')
+seasons_label = CTkLabel(sidebar, text='Select Season', text_color='black')
 seasons_label.pack()
 
+#Create Radio Buttons
 seasons = ['Summer', 'Autumn', 'Winter', 'Spring', 'None']
 
+#make the default selected season None
 selected_season = StringVar(value='None')
 
+#loop that creates all radio buttons
 for season in seasons:
-    radio = Radiobutton(sidebar, text=season, value=season, variable=selected_season)
+    radio = CTkRadioButton(sidebar, text=season, value=season, variable=selected_season, text_color='black')
     radio.pack()
 
-random_tree_button = Button(sidebar, text="Create Crazy Tree", command=lambda: generate_tree(canvas, 400, 520, -90, 300, 10, 0.5, random_tree_button, root, {'scale':slider.get(),
-                                                                                                                                                  'randomization':random.randint(100, 150),
-                                                                                                                                                    'season':selected_season.get()}))
+#values passed when crazy tree is created
+random_tree_values = { 'scale':70,
+                        'randomization':random.randint(100, 150),
+                        'season':random.choice(seasons),
+                        'depth': 10
+                        }
+
+#Create Random Tree Button
+random_tree_button = CTkButton(sidebar, text="Create Crazy Tree", command=lambda: generate_tree(canvas, starting_tree_values['x1'], starting_tree_values['y1'], starting_tree_values['angle'], starting_tree_values['length'], random_tree_button, root, random_tree_values))
 random_tree_button.pack()  
+
+# Create a label to display the season message
+message_label = CTkLabel(root, text="")
+message_label.pack()
+
+def reset_to_default():
+    depth_slider.set(100)
+    scale_slider.set(70)
+    randomization_slider.set(45)
+    selected_season = StringVar(value='None')
+
+#reset everyhting to default button
+reset_to_default_button = CTkButton(sidebar, text='Reset to Default', command=lambda:reset_to_default())
+reset_to_default_button.pack()
 
 root.mainloop()
