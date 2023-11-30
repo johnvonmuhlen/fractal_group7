398b88996ab6098543c7fe656f5564c649af0dac
Md Lokman Hosan <76894243+hlwlokman@users.noreply.github.com>
2023-11-30 12:46:38 +0100

    default button
    default option changed

--- main.py
+++ main.py
@@ -33,19 +33,42 @@ canvas.bind('<Button-3>', zoom_out)
 slider_label = Label(sidebar, text='Choose tree scale')
 slider_label.pack()
 
+<<<<<<< Updated upstream
 #Slider to make tree larger/shorter
 slider = Scale(sidebar, from_=0, to=100, orient='horizontal')
 slider.set(70)
 slider.pack()
+=======
+#Slider to choose depth
+depth_slider = CTkSlider(sidebar, from_=20, to=150, orientation='horizontal')
+depth_slider.set(80)
+depth_slider.pack()
+>>>>>>> Stashed changes
 
 #Slider Label
 slider_label = Label(sidebar, text='Randomize Tree')
 slider_label.pack()
 
+<<<<<<< Updated upstream
 #Slider to make tree larger/shorter
 slider_2 = Scale(sidebar, from_=0, to=100, orient='horizontal')
 slider_2.set(45)
 slider_2.pack()
+=======
+#Slider to choose scale
+scale_slider = CTkSlider(sidebar, from_=40, to=100, orientation='horizontal')
+scale_slider.set(60)
+scale_slider.pack()
+
+#randomization Slider Label
+randomization_slider_label = CTkLabel(sidebar, text='Randomize Tree', text_color='black')
+randomization_slider_label.pack()
+
+#Slider to choose randomization
+randomization_slider = CTkSlider(sidebar, from_=11, to=100, orientation='horizontal')
+randomization_slider.set(45)
+randomization_slider.pack()
+>>>>>>> Stashed changes
 
 #Seasons Label
 seasons_label = Label(sidebar, text='Select Season')
@@ -59,9 +82,51 @@ for season in seasons:
     radio = Radiobutton(sidebar, text=season, value=season, variable=selected_season)
     radio.pack()
 
+<<<<<<< Updated upstream
 random_tree_button = Button(sidebar, text="Create Crazy Tree", command=lambda: generate_tree(canvas, 400, 520, -90, 300, 10, 0.5, random_tree_button, root, {'scale':slider.get(),
                                                                                                                                                   'randomization':random.randint(100, 150),
                                                                                                                                                     'season':selected_season.get()}))
 random_tree_button.pack()  
+=======
+#loop that creates all radio buttons
+for i in range(len(seasons)):
+    radio = CTkRadioButton(sidebar, text=seasons[i], value=seasons[i], variable=selected_season, text_color='black')
+    radio.pack(pady=5)
+    radio_buttons.append(radio)
+
+#values passed when crazy tree is created
+random_tree_values = { 'scale':70,
+                        'randomization':random.randint(100, 150),
+                        'season':random.choice(seasons),
+                        'depth': 10
+                        }
+
+#function to randomize settings
+def randomize_settings():
+    depth_slider.set(random.randint(20, 100))
+    scale_slider.set(random.randint(40, 100))
+    randomization_slider.set(random.randint(11, 100))
+    radio_buttons[random.randint(0, 4)].select()
+
+#Create Random Tree Button
+randomize_settings_button = CTkButton(sidebar, text="Randomize Settings", command=lambda:randomize_settings())
+randomize_settings_button.pack(pady=20)  
+
+# Create a label to display the season message
+message_label = CTkLabel(root, text="")
+message_label.pack()
+
+def reset_to_default():
+    depth_slider.set(80)
+    scale_slider.set(60)
+    randomization_slider.set(45)
+    radio_buttons[4].select()
+
+#reset everyhting to default button
+reset_to_default_button = CTkButton(sidebar, text='Reset to Default', command=lambda:reset_to_default())
+reset_to_default_button.pack()
+
+CTkLabel(sidebar, text='Zoom In/Out: Right & Left Click', text_color='black', font=('Arial', 11)).pack()
+>>>>>>> Stashed changes
 
 root.mainloop()
